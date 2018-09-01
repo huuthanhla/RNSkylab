5c1adf673843cf5b311c9b424287c0c0d70ff609
thanhlazio <thanhlh@lifetimetech.vn>
2018-09-01 09:34:14 +0700

    image components

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Image, View} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,14 +16,16 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component{
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Image 
+          resizeMode='contain'
+          style={{ width: 200, height: 200, borderRadius: 100, borderWidth: 0.5, }} 
+          source={{ uri: 'https://www.planwallpaper.com/static/images/112131.jpg' }}
+        />
       </View>
     );
   }
