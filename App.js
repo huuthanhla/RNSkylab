21913fe229f980b02b566b7ffb5e4c91e2296c50
thanhlazio <thanhlh@lifetimetech.vn>
2018-09-02 11:03:55 +0700

    How to redux working

--- App.js
+++ App.js
@@ -7,7 +7,81 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  Platform, 
+  StyleSheet, 
+  Text,
+  View
+} from 'react-native';
+
+import {createStore} from 'redux'
+
+// State
+let appState = { number: 1, histories: [1] }
+
+// Action
+const add = {
+  type: 'ADD',
+  value: 1
+}
+
+const sub = {
+  type: 'SUB',
+  value: 1
+}
+
+// Reducer
+const numberReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD':
+      // Mutable state
+      // state.number += action.value
+
+      const addValue = state.number + action.value
+      // Immutable state
+      state = {
+        ...state,
+        histories: [...state.histories, addValue],
+        number: addValue
+      }
+      break
+
+    case 'SUB':
+      // Immutable state
+      const subValue = state.number - action.value
+      state = {
+        ...state,
+        histories: [...state.histories, subValue],
+        number: subValue
+      }
+      break
+  }
+
+  return state
+}
+
+// Store
+const store = createStore(numberReducer, appState)
+
+// Test
+store.subscribe( () => {
+  console.log('State updated', store.getState())
+})
+store.dispatch(add)
+store.dispatch(add)
+store.dispatch(add)
+store.dispatch(sub)
+
+store.dispatch({
+  type: 'ADD',
+  value: 5
+})
+
+const createAddAction = (number) => {
+  return {type: 'ADD', value: number}
+}
+
+store.dispatch( createAddAction(100) )
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
